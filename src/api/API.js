56e993db0f6882bf68a2fe31667bb0c5caedb73f
Pedro De Nigris Vasconcellos <pedronvas18@gmail.com>
2018-06-19 21:20:32 -0300

    Fixed back end persistence

--- src/api/API.js
+++ src/api/API.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
-const authenticationToken = Math.random()
-  .toString(36)
-  .substr(-8);
+let authenticationToken = localStorage.token;
+if (!authenticationToken)
+  authenticationToken = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8);
 
 const instance = axios.create({
   baseURL: "http://localhost:3001",
   timeout: 100,
   headers: {
-    Authorization: authenticationToken,
+    Authorization: "whatever",
     Accept: "application/json"
   }
 });
